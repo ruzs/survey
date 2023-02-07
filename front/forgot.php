0aaaa0676223beb2b5cace117dc433dd17a74782
ruzs <eddie10913@gmail.com>
2023-02-08 00:40:02 +0800

    Add forgot pw

---
+++ front/forgot.php
@@ -0,0 +1,18 @@
+<h1 class="text-center fw-bold" style="margin-top:200px;">查詢密碼</h1>
+  <div class="text-center">
+    <div class="mt-5">請輸入信箱以查詢密碼</div>
+    <div class="my-2"><input type="text" name="email" id="email"></div>
+    <div class="my-5 text-success fw-bold fs-4" id="result"></div>
+    <div class="my-5"><button class="btn btn-primary" onclick='forgot()'>尋找</button></div>
+  </div>
+
+
+<script>
+  function forgot() {
+    $.get('./api/forgot.php', {
+      email: $("#email").val()
+    }, (result) => {
+      $("#result").html(result)
+    })
+  }
+</script>
